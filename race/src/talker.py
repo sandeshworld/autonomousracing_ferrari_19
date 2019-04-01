518d8400e91e78c9c7538c9eedd8a8ce60f81040
Austin Sullivan <acs3ss@virginia.edu>
2019-04-01 17:13:33 -0400

    changed atlked formula to reflect changes

--- race/src/talker.py
+++ race/src/talker.py
@@ -17,8 +17,8 @@ varP = drive_values()
 def fnc_callback(msg):
     # mapping function
     global varP
-    varP.pwm_drive = int((msg.velocity * 32.77) + 9831)
-    varP.pwm_angle = int((msg.angle * 32.77) + 9831)
+    varP.pwm_drive = int((msg.velocity * 32.76) + 9831)
+    varP.pwm_angle = int((msg.angle * 32.76) + 9831)
     
 
 if __name__=='__main__':
