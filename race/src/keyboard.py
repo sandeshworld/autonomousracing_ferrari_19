2d766a6a238e461fdfbd29a8b43984b570ccb6ec
sandeshworld <sb3vf@virginia.edu>
2019-04-02 19:20:37 -0400

    final updates for demo

--- race/src/keyboard.py
+++ race/src/keyboard.py
@@ -10,11 +10,12 @@ stdscr = curses.initscr()
 curses.cbreak()
 stdscr.keypad(1)
 rospy.init_node('keyboard_talker', anonymous=True)
-pub = rospy.Publisher('drive_parameters', drive_param, queue_size=10)
+pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
 stdscr.refresh()
 
-delta = None
+forward_delta = 1
+left_delta = 10
 
 key = ''
 while key != ord('q'):
@@ -24,27 +25,36 @@ while key != ord('q'):
         if key == ord(' '):  # this key will center the steer and throttle
             forward = 0
             left = 0
-            delta = None
-        elif key == curses.KEY_UP or key == curses.KEY_DOWN or key == curses.KEY_LEFT or key == curses.KEY_RIGHT:
-            if delta is None:
-                delta = 1  # first press of a movement key
+        elif key == curses.KEY_UP:
+            if -7 < forward < 7:
+                forward = 7
+            elif forward + forward_delta >= 100:
+		forward = 100
             else:
-                delta += 1  # building on previous key presses
-
-            if key == curses.KEY_UP:
-                forward += delta 
-            elif key == curses.KEY_DOWN:
-                forward -= delta
-            if key == curses.KEY_LEFT:
-                left += delta
-            elif key == curses.KEY_RIGHT:
-                left -= delta
+                forward += forward_delta 
+        elif key == curses.KEY_DOWN:
+            if -7 < forward < 7:
+                forward = -7
+            elif forward - forward_delta <= -100:
+		forward = -100
+            else:
+                forward -= forward_delta 
+        elif key == curses.KEY_LEFT:
+            if left - left_delta <= -100:
+                left = -100
+            else:
+                left -= left_delta
+        elif key == curses.KEY_RIGHT:
+            if left + left_delta >= 100:
+                left = 100
+            else:
+                left += left_delta
         else:
-            delta = None
+            print("Invalid key press")
 
 	msg = drive_param()
-	msg.velocity = forward if -100 <= forward <= 100 else -100 if forward < 0 else 100
-	msg.angle = left if -100 <= left <= 100 else -100 if left < 0 else 100
+	msg.velocity = forward
+	msg.angle = left 
 	pub.publish(msg)
 
 curses.endwin()
--- race/src/talker.py
+++ race/src/talker.py
@@ -3,7 +3,8 @@
 import rospy
 from race.msg import drive_values
 from race.msg import drive_param
-
+import subprocess
+import signal
 
 """
 What you should do:
@@ -12,23 +13,52 @@ What you should do:
  3. Publish the calculated PWM values on topic "drive_pwm" using custom message drive_values
 """
 
+class TimeoutException(Exception):
+    pass
+
+
+def timeout_handler(signum, frame):
+    raise TimeoutException
+
+
+signal.signal(signal.SIGALRM, timeout_handler)
 varP = drive_values()
+varP.pwm_drive = 9830
+varP.pwm_angle = 9830
+
+
+def check_connection():
+    address = '192.168.1.31'
+    signal.alarm(1)
+    try:
+        res = subprocess.call(['ping','-c','1',address])
+        return res == 0
+    except TimeoutException:
+        return False
+
 
 def fnc_callback(msg):
     # mapping function
     global varP
-    varP.pwm_drive = int((msg.velocity * 32.77) + 9831)
-    varP.pwm_angle = int((msg.angle * 32.77) + 9831)
-    
-
+    varP.pwm_drive = int((msg.velocity * 32.76) + 9830)
+    varP.pwm_angle = int((msg.angle * 32.76) + 9830)
+        
 if __name__=='__main__':
+    k = 1
     rospy.init_node('pub_n_sub')
 
     pub=rospy.Publisher('drive_pwm', drive_values, queue_size=1)
     sub=rospy.Subscriber('drive_parameters', drive_param, fnc_callback)
-    rate=rospy.Rate(10)
-
+    rate=rospy.Rate(60)
 
     while not rospy.is_shutdown():
+        if k % 20 == 0:
+            if not check_connection():
+                varP.pwm_drive=9830
+                varP.pwm_angle=9830
+                pub.publish(varP)
+                exit()
+        k += 1
 	pub.publish(varP)
         rate.sleep()
+
