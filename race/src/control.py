17c35c587d021aea71da6e04e2625e0fcd7f4846
Austin Sullivan <acs3ss@virginia.edu>
2019-04-15 13:56:18 -0400

    outline for A4

--- race/src/control.py
+++ race/src/control.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-
+import math
 import rospy
 from race.msg import drive_param
 from race.msg import pid_input
 
 kp = 14.0
 kd = 0.09
-servo_offset = 18.5	# zero correction offset in case servo is misaligned. 
-prev_error = 0.0 
+servo_offset = 18.5	# zero correction offset in case servo is misaligned.
+prev_error = 0.0
 vel_input = 25.0	# arbitrarily initialized. 25 is not a special value. This code can input desired velocity from the user.
+angle = 90
 
 pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
@@ -17,17 +18,25 @@ def control(data):
 	global vel_input
 	global kp
 	global kd
+	global angle
 
 	## Your code goes here
-	# 1. Scale the error 
+	# 1. Scale the error
+	error = data.pid_error
 	# 2. Apply the PID equation on error to compute steering
+	v0 = (kp*error) + kd*(prev_error - error)
 	# 3. Make sure the steering value is within bounds for talker.py
- 	
+
+	prev_error = error
+	angle = angle - v0
+
+	angle = angle if -100 < angle < 100 else 100 if angle > 100 else -100
+	vel_input = vel_input if -100 < vel_input < 100 else 100 if vel_input > 100 else -100
 
 	## END
 
 	msg = drive_param();
-	msg.velocity = vel_input	
+	msg.velocity = vel_input
 	msg.angle = angle
 	pub.publish(msg)
 
